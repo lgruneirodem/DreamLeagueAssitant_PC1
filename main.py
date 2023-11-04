a5a630b0f873e1027bf2b8c0cbcaed422e442594
raqgd <99642900+raqgd@users.noreply.github.com>
2023-11-04 15:24:02 +0100

    try pop-up

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
-import json
+import csv
+import re
 
 #Establecemos la página que queremos abrir
 driver = webdriver.Chrome()
@@ -12,7 +13,7 @@ url = 'https://es.whoscored.com/Regions/206/Tournaments/4/Seasons/9682/Stages/22
 #Abrir adblock
 options = webdriver.ChromeOptions()
 options.add_argument('--ignore-certificate-errors')
-options.add_argument('load-extension=' + r"C:\Users\Raque\OneDrive\Documentos\Uni\3º\Proyecto I\webscrapping\adblocker")
+options.add_argument('load-extension=' + r"C:\Users\Raque\OneDrive\Documentos\Uni\3º\Proyecto I\pythonProject\DreamLeagueAssitant_PC1\adblocker")
 driver = webdriver.Chrome(options=options)
 driver.maximize_window()
 
@@ -33,53 +34,62 @@ wait = WebDriverWait(driver, 10)
 boton = driver.find_element(By.CLASS_NAME, 'css-1wc0q5e')
 boton.click()
 
-time.sleep(2)
-
 #Cerrar pop-up inicial
-boton2 = driver.find_element(By.CLASS_NAME, 'webpush-swal2-close')
-boton2.click()
+try:
+    boton2 = driver.find_element(By.CLASS_NAME, 'webpush-swal2-close')
+    boton2.click()
+except:
+    pass
 
 #Seleccionar todos los jugadores
 boton_jgdr = driver.find_element(By.XPATH, '/html/body/div[5]/div[5]/div[2]/div[1]/div[2]/dl[2]/dd[2]/a')
 boton_jgdr.click()
+omitir_filas = 2  # Número de filas a omitir al inicio de cada página
 
-print("Vamos a imprimir las estadísticas de los jugadores")
 
-data = []  # Lista para almacenar la información en cada iteración
-#for i in range(150):
-# Extraer el resumen de partidos
-texto = driver.find_element(By.ID, 'statistics-table-summary').text
+# Crear un archivo CSV para escribir
+with open('datos.csv', mode='w', newline='', encoding='utf-8') as csv_file:
+    csv_writer = csv.writer(csv_file)
 
-# Dividir el texto en partes separadas por espacios
-valores = texto.split()
+    for i in range(150):
+        # Extraer el resumen de partidos
+        texto = driver.find_element(By.ID, 'statistics-table-summary')
+        text_lines = texto.text.split('\n')
 
-# Crear un diccionario con las claves específicas y sus valores
-summary_data = {
-    'Jgdos': valores[0],
-    'Mins': valores[1],
-    'Goles': valores[2],
-    'Asist': valores[3],
-    'Amar': valores[4],
-    'Roja': valores[5],
-    'TpP': valores[6],
-    'AP%': valores[7],
-    'Aéreos': valores[8],
-    'JdelP': valores[9],
-    'Rating': valores[10]
-}
+        for line_idx, line in enumerate(text_lines):
+            if omitir_filas > 0:
+                omitir_filas -= 1
+                continue
 
-# Agregar el diccionario a la lista de datos
-data.append(summary_data)
+            # Reemplazar "-" por "0" y eliminar espacios en blanco adicionales
+            line = re.sub(r'\s+', ' ', line.replace('-', '0'))
 
-#Escribir los datos recopilados en un archivo JSON
-with open('datos_partidos.json', 'w') as file:
-    json.dump(data, file, indent=4)
+            # Utilizar una expresión regular para dividir el texto en columnas y mantener las palabras compuestas unidas
+            columns = re.split(r'(?<!\S)(?=\d)|,', line)
 
-print('\n')
-print("Vamos a imprimir las estadísticas de los equipos")
+            # Filtrar solo caracteres alfanuméricos y espacios en cada columna
+            for col_idx, column in enumerate(columns):
+                columns[col_idx] = ''.join([c if c.isalnum() or c.isspace() else '' for c in column])
 
-botonEquipos = driver.find_element_by_xpath('/html/body/div[5]/div[3]/div[2]/ul/li[3]/a')
-botonEquipos.click()
+            # Eliminar la coma al inicio de la fila
+            if columns[0] == '':
+                columns.pop(0)
 
-#infoEquipos = driver.find_elements_by_xpath('/html/body/div[5]/div[6]/div[2]/div[3]/div')
-#print(infoEquipos.text)
+            # Escribir la fila en el archivo CSV
+            csv_writer.writerow(columns)
+
+        try:
+            # Esperar hasta que el botón "Siguiente" sea interactuable
+            wait = WebDriverWait(driver, 10)
+            boton_siguiente = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[5]/div[5]/div[2]/div[4]/div/dl[2]/dd[3]/a')))
+
+            # Hacer clic en el botón "Siguiente"
+            boton_siguiente.click()
+            omitir_filas = 2  # Restablecer el contador para la siguiente página
+        except:
+            pass  # Si el botón "Siguiente" no se encuentra o no es interactuable, pasa a la siguiente iteración
+
+
+
+# Cierra el navegador al final del bucle
+driver.quit()
